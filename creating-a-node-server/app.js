bb77f6406b996ee54891c4cee4965a88b43dae34
couvq <quentincouvelaire@gmail.com>
2022-08-15 14:36:05 -0400

    refactored for blocking code

--- creating-a-node-server/app.js
+++ creating-a-node-server/app.js
@@ -15,20 +15,20 @@ const server = http.createServer((req, res) => {
         return res.end();
     }
 
-    if(url === '/message' && method === 'POST') {
+    if (url === '/message' && method === 'POST') {
         const body = [];
         req.on('data', (chunk) => {
             console.log(chunk);
             body.push(chunk);
         });
-        req.on('end', () => {
+        return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
             fs.writeFileSync('message.txt', message);
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
         });
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
     }
 
     res.setHeader('Content-Type', 'text/html');
