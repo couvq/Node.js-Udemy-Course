72c410e8613a991b01eb9879ee61ed2ab6b8136d
couvq <quentincouvelaire@gmail.com>
2022-08-15 12:15:31 -0400

    first server is set up

---
+++ creating-a-node-server/app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method, req.headers);
+    // process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write(`
+        <html> 
+            <head><title>My Node.js App</title></head>
+            <body><h1>Hello from my Node.js Server!</h1></body>
+        </html>
+    `);
+    res.end();
+});
+
+server.listen(3000);
