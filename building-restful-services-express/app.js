fdc53196d3da74987bf8f048ee31276e538d2b6c
couvq <quentincouvelaire@gmail.com>
2022-08-20 12:39:39 -0400

    misc error message

--- building-restful-services-express/app.js
+++ building-restful-services-express/app.js
@@ -33,7 +33,7 @@ app.get('/api/courses/:id', (req, res) => {
         res.send(matchingCourse);
     } else {
         // we did not find the course with given id!
-        res.send(`Sorry! We couldn't find a course with that id.`);
+        res.send(`Sorry! We couldn't find a course with id of ${id}.`);
     }
 });
 
