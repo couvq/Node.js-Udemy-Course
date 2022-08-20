67094c107232bee2bce9781c5a8aa6bfb228102b
couvq <quentincouvelaire@gmail.com>
2022-08-20 14:32:04 -0400

    input validation logic for post

--- building-restful-services-express/app.js
+++ building-restful-services-express/app.js
@@ -41,6 +41,12 @@ app.get('/api/courses/:id', (req, res) => {
 
 // POST request to create a new course
 app.post('/api/courses', (req, res) => {
+    if(!req.body.name || req.body.name.length < 3) {
+        // 400 Bad Request
+        res.status(400).send('Name is required and should be minimum 3 characters.');
+        return; // don't execute rest of this function
+    }
+
     const course = {
         id: courses.length + 1,
         name: req.body.name
