256a579cfd60a1f3be7d5eb0c650e1c901f72701
couvq <quentincouvelaire@gmail.com>
2022-08-20 12:34:14 -0400

    implemented get course by id endpoint

--- building-restful-services-express/app.js
+++ building-restful-services-express/app.js
@@ -1,17 +1,40 @@
 const express = require('express');
 const app = express();
 
+// data will be dummy as an array for now just to practice with express
+const courses = [
+    { id: 1, name: 'Course1'},
+    { id: 2, name: 'Course2'},
+    { id: 3, name: 'Course3'}
+];
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
 });
 
 // /api/courses/1
 app.get('/api/courses/:id', (req, res) => {
-    res.send(req.params);
+    // find course with given id
+    const id = parseInt(req.params.id);
+
+    let matchingCourse = null;
+
+    for(let course of courses) {
+        if(course.id === id) {
+            matchingCourse = course;
+        }
+    }
+
+    if(matchingCourse !== null) {
+        res.send(matchingCourse);
+    } else {
+        // we did not find the course with given id!
+        res.send(`Sorry! We couldn't find a course with that id.`);
+    }
 });
 
 const port = process.env.PORT || 3000;
