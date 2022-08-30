043750f8ab60ece92ef7fbaf9a4103239335735d
couvq <quentincouvelaire@gmail.com>
2022-08-30 15:12:37 -0400

    refactored to use express rather than http module

--- working-with-express/app.js
+++ working-with-express/app.js
@@ -1,4 +1,3 @@
-const http = require('http');
 const express = require('express');
 
 const app = express();
@@ -13,6 +12,6 @@ app.use((req, res, next) => {
     res.send('<h1>Hello from Express!</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000, () => {
+    console.log('listening on port 3000.');
+});
