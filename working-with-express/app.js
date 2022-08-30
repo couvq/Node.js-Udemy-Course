4a00ba9ce10554417a4649ea797f2fd213aa54ab
couvq <quentincouvelaire@gmail.com>
2022-08-30 16:51:13 -0400

    playing around with app object

--- working-with-express/app.js
+++ working-with-express/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use('/add-product', (req, res, next) => {
-    res.send('This is the add product page')
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
